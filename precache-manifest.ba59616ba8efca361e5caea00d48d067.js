37c2cf52fd602c65d8069df5bfe3a705c47e9810
Bishwas Thapa <bishwastmgr@gmail.com>
2019-09-13 12:29:30 +0545

    Updates

---
+++ precache-manifest.ba59616ba8efca361e5caea00d48d067.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ae13607074e4933fe15bede10bfd83f",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "256fdd4cc5c62fc76328",
+    "url": "/react-app/static/css/main.334e67e7.chunk.css"
+  },
+  {
+    "revision": "fb5ae75bd03601c580a2",
+    "url": "/react-app/static/js/2.da31070e.chunk.js"
+  },
+  {
+    "revision": "256fdd4cc5c62fc76328",
+    "url": "/react-app/static/js/main.66eb47a3.chunk.js"
+  },
+  {
+    "revision": "d0a653c1c78937ec0799",
+    "url": "/react-app/static/js/runtime~main.544f23ae.js"
+  }
+]);
